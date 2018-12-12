df0f7bd986e25fe26953e7bf0fd55bf1457d50f2
finfanterodal <finfanterodal@nautilus11.danielcastelao.org>
2018-12-12 11:46:33 +0100

    Boletin_14

---
+++ Boletin14_V2/src/boletin14/Boletin14.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin14;
+
+/**
+ *
+ * @author finfanterodal
+ */
+public class Boletin14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ConversorTemperaturas conversor1 = new ConversorTemperaturas();
+        conversor1.menu();
+    }
+}
